f4af69e93b1539f8e7f19f49592700e8d25be61e
admin <admin@pyindex.com>
2017-03-14 10:15:21 +0800

    Admin the first commit

---
+++ runserver.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Longgeek <longgeek@fuvism.com>
+
+from fuerte import app
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
