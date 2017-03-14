449045799412dce2df938555f46d2513fdd228b4
longgeek <longgeek@fuvism.com>
2017-03-14 10:22:05 +0800

    add the api dirs
    Change-Id: Ia92f9b17635869e2d6abd81f60ffa131c393a99f

---
+++ fuerte/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Longgeek <longgeek@fuvism.com>
+
+"""Fuerte Porject
+
+Flask based.
+"""
+from flask import Flask
+from flask import jsonify
+from flask_restful import Api
+# from looker.api.v1 import controller as v1_controller
+
+# Flask Service App
+app = Flask(__name__)
+
+# API Service config
+apiroute = Api(app)
+# apiroute.add_resource(v1_controller.APIView, '/api/v1')
+
+
+@app.errorhandler(404)
+def http_404(error):
+    return jsonify(http_code=404,
+                   message="Page/API url path not found!"), 404
+
+
+@app.errorhandler(500)
+def http_500(error):
+    return jsonify(http_code=500,
+                   message="Runtime error(HTTP 500)!"), 500
+
+
+# app.wsgi_app = ProxyFix(app.wsgi_app)
