d66f1902483954c6adac2cdc9434d7ee890a5990
longgeek <longgeek@fuvism.com>
2017-03-24 14:41:32 +0800

    update the ceph.sh script
    Change-Id: I86be51d733eb41f38bce52ea63b4675f008f8765

--- scripts/ceph.sh
+++ scripts/ceph.sh
@@ -2,18 +2,20 @@
 #http://www.aboutyun.com/thread-10662-1-1.html
 
 apt-get install -y ceph ceph-deploy ceph-fs-common ceph-fuse
-ceph-deploy new node01
-ceph-deploy install node01
-ceph-deploy mon create node01
-ceph-deploy mds create node01
-ceph-deploy gatherkeys node01
-ceph-deploy osd prepare node01:/srv/ceph/osd0
-ceph-deploy osd prepare node01:/srv/ceph/osd1
-ceph-deploy osd activate node01:/srv/ceph/osd0
-ceph-deploy osd activate node01:/srv/ceph/osd1
-ceph-deploy admin node01
+cd /etc/ceph
+ceph-deploy new dc-manager01 dc-manager02 dc-manager03
+ceph-deploy install dc-manager01 dc-manager02 dc-manager03
+ceph-deploy mon create dc-manager01 dc-manager02 dc-manager03
+ceph-deploy gatherkeys dc-manager01 dc-manager02 dc-manager03
+ceph-deploy mds create dc-manager01 dc-manager02 dc-manager03
+mkdir -p /ceph/osd
+echo "/dev/vg-ceph-storage/lv-ceph-storage  /ceph/osd xfs  rw,relatime,attr2,inode64,prjquota  0  0" >> /etc/fstab
+ceph-deploy osd prepare dc-manager01:/ceph/osd dc-manager02:/ceph/osd dc-manager03:/ceph/osd
+ceph-deploy osd activate dc-manager01:/ceph/osd dc-manager02:/ceph/osd dc-manager03:/ceph/osd
+ceph-deploy admin dc-manager01
 
 ceph -s
 ceph osd trea
 restart ceph-all
-#mount.ceph manage01,node01,node02:/ /mnt -o name=admin,secret=AQC50tNYCCWlBRAAdy1epXR6OX3vzkIf4uVxPg==
+#mount.ceph dc-manager01,dc-manager02,dc-manager03:/ /mnt -o name=admin,secret=AQC50tNYCCWlBRAAdy1epXR6OX3vzkIf4uVxPg==
+#echo "dc-manager01,dc-manager02,dc-manager03:/        /storage ceph name=admin,secret=AQCxodRYAKBDIRAAv+QTchsG1jM1I0EzP4r2yA== 0      0" >> /etc/fstab
