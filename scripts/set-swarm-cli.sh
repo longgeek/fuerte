153e08379d119c62d55e1e1fdedd765ba3be8caa
longgeek <longgeek@fuvism.com>
2017-03-26 01:24:15 +0800

    add the create container tests script
    Change-Id: Ic38581aa9bd6b02c9593e57fbc6648b7d7dac77e

--- scripts/set-swarm-cli.sh
+++ scripts/set-swarm-cli.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 [ -e ~/.docker ] || mkdir ~/.docker
-cp /etc/docker/certs.d/ca/ca.pem ~/.docker/
-cp /etc/docker/certs.d/client/client.pem ~/.docker/cert.pem
-cp /etc/docker/certs.d/client/client-key.pem ~/.docker/key.pem
+cp /storage/services/ca/ca.pem ~/.docker/
+cp /storage/services/client/client.pem ~/.docker/cert.pem
+cp /storage/services/client/client-key.pem ~/.docker/key.pem
 
 docker --tlsverify -H:4000 ps
---
+++ tests/create_container.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import requests
+import simplejson as json
+from requests.packages.urllib3.exceptions import SNIMissingWarning
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+from requests.packages.urllib3.exceptions import InsecurePlatformWarning
+
+requests.packages.urllib3.disable_warnings(SNIMissingWarning)
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+requests.packages.urllib3.disable_warnings(InsecurePlatformWarning)
+
+params = {
+    "action": "Container:Create",
+    "params": {
+        "username": "longgeek",
+        "image": "192.168.0.2:5000/longgeek/ubuntu-14.04.1:base"
+    }
+}
+r = requests.post(
+    url="http://192.168.0.2:8000/api/v1",
+    headers={"content-type": "application/json"},
+    data=json.dumps(params),
+)
+
+print r.json()
