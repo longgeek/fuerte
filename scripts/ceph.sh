66a0ee3dbca248072acac81ab63ff72452e7f71a
longgeek <longgeek@fuvism.com>
2017-04-08 00:25:03 +0800

    bugfix
    Change-Id: I150fe9a54f9d55c25cb8e80ec323047653061a20

--- scripts/ceph.sh
+++ scripts/ceph.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #http://www.aboutyun.com/thread-10662-1-1.html
 
-echo "deb https://download.ceph.com/debian-jewel/ trusty main" > /etc/apt/sources.list.d/ceph.list
+echo "deb http://mirrors.aliyun.com/ceph/debian-jewel/ trusty main" > /etc/apt/sources.list.d/ceph.list
 apt-get update
 apt-get install -y --force-yes ceph ceph-deploy ceph-fs-common ceph-fuse ceph-mds
 cd /etc/ceph
@@ -11,6 +11,7 @@ ceph-deploy mon create dc-manager01 dc-manager02 dc-manager03
 ceph-deploy gatherkeys dc-manager01 dc-manager02 dc-manager03
 ceph-deploy mds create dc-manager01 dc-manager02 dc-manager03
 mkdir -p /ceph/osd
+chown ceph:ceph /ceph/osd
 echo "/dev/vg-ceph-storage/lv-ceph-storage  /ceph/osd xfs  rw,relatime,attr2,inode64,prjquota  0  0" >> /etc/fstab
 ceph-deploy osd prepare dc-manager01:/ceph/osd dc-manager02:/ceph/osd dc-manager03:/ceph/osd
 ceph-deploy osd activate dc-manager01:/ceph/osd dc-manager02:/ceph/osd dc-manager03:/ceph/osd
@@ -27,7 +28,7 @@ echo "mon clock drift allowed = 1" >> ceph.conf
 ceph-deploy --overwrite-conf config push dc-manager01 dc-manager02 dc-manager03
 
 ceph -s
-ceph osd trea
+ceph osd tree
 restart ceph-all
 #mount.ceph dc-manager01,dc-manager02,dc-manager03:/ /storage -o name=admin,secret=AQC50tNYCCWlBRAAdy1epXR6OX3vzkIf4uVxPg==
 #echo "dc-manager01,dc-manager02,dc-manager03:/        /storage ceph name=admin,secret=AQCxodRYAKBDIRAAv+QTchsG1jM1I0EzP4r2yA== 0      0" >> /etc/fstab
