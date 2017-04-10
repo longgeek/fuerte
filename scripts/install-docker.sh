74584b96b343468e6d1412236defcf23d27a24cb
longgeek <longgeek@fuvism.com>
2017-04-10 10:37:17 +0800

    install docker use aliyun sources
    Change-Id: I1e02fda4b463d8dbb1fa4a26a8950fda36ca6844

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get install -y apt-transport-https ca-certificates
-apt-key adv \
-        --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-        --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
-apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
-# apt-get install -y --force-yes docker-engine
-dpkg -i /opt/git/fuerte/install/packages/docker-engine*.deb
-apt-get -f -y --force-yes install
+curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -
