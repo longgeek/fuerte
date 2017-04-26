2147792e3b0e84de1f97fb254979533446f43987
longgeek <longgeek@fuvism.com>
2017-04-26 16:57:02 +0800

    update
    Change-Id: I144a943cbc7cd23c77196001ca54840cd481dbed

--- scripts/ceph.sh
+++ scripts/ceph.sh
@@ -7,6 +7,12 @@ apt-get install -y --force-yes ceph ceph-deploy ceph-fs-common ceph-fuse ceph-md
 cd /etc/ceph
 ansible dc -a "chown -R ceph:ceph /ceph"
 ceph-deploy new dc-manager01 dc-manager02 dc-manager03
+##############################################
+# 单节点安装需要执行下面步骤,
+# 同时无需安装 mds
+# echo "osd crush chooseleaf type = 0" >> /etc/ceph/ceph.conf
+# echo "osd pool default size = 1" >> /etc/ceph/ceph.conf
+##############################################
 ceph-deploy install dc-manager01 dc-manager02 dc-manager03
 ceph-deploy mon create dc-manager01 dc-manager02 dc-manager03
 ceph-deploy gatherkeys dc-manager01 dc-manager02 dc-manager03
@@ -17,9 +23,9 @@ ceph-deploy admin dc-manager01
 
 ###############################################
 # 版本大于 0.8 需要手工创建 metadata
-ceph osd pool create cephfs_data 0
-ceph osd pool create cephfs_metadata 0
-ceph fs new cephfs cephfs_metadata cephfs_data
+# ceph osd pool create cephfs_data 0
+# ceph osd pool create cephfs_metadata 0
+# ceph fs new cephfs cephfs_metadata cephfs_data
 ###############################################
 
 echo "mon clock drift allowed = 1" >> ceph.conf
